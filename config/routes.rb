ed0d82bdf5750f919dd98ae531e39e68bd003f66
gregaswright <greg.a.s.wright@gmail.com>
2020-12-01 10:51:23 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :listings
+      resources :carts
+      resources :items
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
